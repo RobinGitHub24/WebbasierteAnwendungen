1125f6f9f1f43d47106dc28e0b86229dee2c71b7
RobinGitHub24 <robin.schaefer1@outlook.de>
2024-10-28 20:11:19 +0100

    Erstes Spring Boot Projekt hinzugefügt

---
+++ WebbasierteAnwendungen/src/main/java/edu/fra/uas/WebbasierteAnwendungen/WebbasierteAnwendungenApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.WebbasierteAnwendungen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbasierteAnwendungenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbasierteAnwendungenApplication.class, args);
+	}
+
+}
